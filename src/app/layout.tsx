85f2e11a5f86d1cbcaaada9cc309ff73216c3bec
Vitor Duarte <vitorduarte.vds7@gmail.com>
2023-06-09 16:03:57 -0300

    fix tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import AppBar from "../components/AppBar";
 import { Providers } from "../components/Providers";
 import "./globals.css";
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
